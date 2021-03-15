a0125a6ef6ec20d3e740e9f7ce612a7d51ad6dfa
binte <namood-e-sahar@myseneca.ca>
2021-03-14 22:30:54 -0400

    Add project files.

---
+++ commonHelpers.h
@@ -0,0 +1,23 @@
+#pragma once
+/*
+	==================================================
+	Assignment #1 Milestone #2
+	==================================================
+	Name   : Namood-e-sehar
+	ID     : 121979207
+	Email  :Namood-e-sahar@myseneca.ca
+	Section:NFF
+*/
+
+#pragma once
+
+#include <stdio.h>
+int currentYear(void);
+void clearStandardInputBuffer(void);
+int getInteger(void);
+int getPositiveInteger(void);
+double getDouble(void);
+double getPositiveDouble(void);
+int getIntFromRange(int lower_bound, int upper_bound);
+char getCharOption(char* string_arr);
+void getCString(char* string, int minimum, int maximum);
